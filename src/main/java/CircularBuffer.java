93a0e255e6b2de654a8804af3cd41732b07d5e51
Pamuta Keawjit <pamutakea@mathregit.group>
2020-08-03 15:15:46 +0700

    [TS2] create_new_buffer_with_default_size_should_10()

--- src/main/java/CircularBuffer.java
+++ src/main/java/CircularBuffer.java
@@ -1,5 +1,14 @@
 public class CircularBuffer {
+    private int bufferSize = 10;
     public boolean isEmpty() {
-        return true;
+        return bufferSize==10;
+    }
+
+    public void writeData(String input) {
+        bufferSize--;
+    }
+
+    public boolean isFull() {
+        return bufferSize == 0;
     }
 }
--- src/test/java/CircularBufferTest.java
+++ src/test/java/CircularBufferTest.java
@@ -9,5 +9,13 @@ public class CircularBufferTest {
         boolean result = cb.isEmpty();
         assertTrue("Buffer not empty", result);
     }
-
+    @Test
+    public void create_new_buffer_with_default_size_should_10(){
+        CircularBuffer cb = new CircularBuffer();
+        for (int i = 0; i < 10; i++) {
+            cb.writeData("A"+i);
+        }
+        boolean result = cb.isFull();
+        assertTrue("Buffer not full",result);
+    }
 }
