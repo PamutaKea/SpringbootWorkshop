1066884f374d9fb5e6e5ba5251b20b9960d3fd4d
Pamuta Keawjit <pamutakea@mathregit.group>
2020-08-04 21:48:29 +0700

    [refector] changed score variable to return in sameScore case

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -20,8 +20,8 @@ public class TennisGame2 {
         boolean sameScoreAll = player1Point == player2Point && player1Point < 3;
         if (sameScoreAll)
         {
-            score = textScores[player1Point];
-            score += "-All";
+            return textScores[player1Point]+"-All";
+
         }
 
         //Deuce
