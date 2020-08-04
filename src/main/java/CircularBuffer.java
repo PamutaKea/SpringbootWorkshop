f04243ab840f813fa23bbe6b6435f76fb472bcce
Pamuta Keawjit <pamutakea@mathregit.group>
2020-08-04 09:54:50 +0700

    Refactor code

--- src/main/java/CircularBuffer.java
+++ src/main/java/CircularBuffer.java
@@ -18,7 +18,7 @@ public class CircularBuffer {
 
     public boolean isEmpty() {
 
-        return bufferSize == 10;
+        return readPointer == writePointer;
     }
 
     public void writeData(String input) {
@@ -27,7 +27,8 @@ public class CircularBuffer {
     }
 
     public boolean isFull() {
-       return bufferSize == 0;
+
+        return writePointer>= bufferSize;
     }
 
     public String readData() {
